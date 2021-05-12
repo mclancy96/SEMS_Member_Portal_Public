2ffcf213b395d86b4d6f34c46158e2c040f93d7b
Mike <mclancy96@gmail.com>
2021-05-12 12:48:31 -0400

    Fixing models

--- models/notification.js
+++ models/notification.js
@@ -1,25 +1,25 @@
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASEURL, {useUnifiedTopology: true, useNewUrlParser: true});
+// const mongoose = require("mongoose");
+// mongoose.connect(process.env.DATABASEURL, {useUnifiedTopology: true, useNewUrlParser: true});
 
-var NotificationSchema = new mongoose.Schema({
-	created: Date,
-	message: String,
-	createdBy: {
-		id: String,
-		name: String
-	}, 
-	accessLevel: {type: String, default: "officer"},
-	effectedMember: {
-		id: String,
-		name: String
-	},
-	expiresAt:{
-		type: Date,
-		default: Date.now() + 2592000000
-	},
-	importance: {type: String, default: "low"},
-	readBy: []
-});
+// var NotificationSchema = new mongoose.Schema({
+// 	created: Date,
+// 	message: String,
+// 	createdBy: {
+// 		id: String,
+// 		name: String
+// 	}, 
+// 	accessLevel: {type: String, default: "officer"},
+// 	effectedMember: {
+// 		id: String,
+// 		name: String
+// 	},
+// 	expiresAt:{
+// 		type: Date,
+// 		default: Date.now() + 2592000000
+// 	},
+// 	importance: {type: String, default: "low"},
+// 	readBy: []
+// });
 
 
-module.exports = mongoose.model("Notification", NotificationSchema);
+// module.exports = mongoose.model("Notification", NotificationSchema);
